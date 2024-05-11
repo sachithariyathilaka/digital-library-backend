df430609102662969bf02035e090b342dde70a23
sachith.vidu@yahoo.com <sachith.vidu@yahoo.com>
2024-05-11 14:54:10 +0530

    Data load function implemented

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Node lambda Tutorial
-Node JS tutorial project from execute CRUD operations hosted in AWS Lambda
+Node.js tutorial project from execute CRUD operations hosted in AWS Lambda.
+
+* ### Technologies
+
+  * Node JS 20.12.11
+  * TypeScript 5.4.5
+  * Mongo DB 6.6.1
+  * Lambda Build 1.0.6
+  * AWS Lambda 8.10.137
+  * ESBuild Register 3.5.0
+  
+* ### Release Note
+
+  * Release: 1.0.0
+  * Date: 2024/05/11
+  * Module: Node lambda Tutorial
+  * Changes
+    * Implement the insert books data function.
+    * Implement the load books data function.
+    * Implement the update books data function.
+    * Implement the delete books data function.
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import {APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2} from "aws-lambda";
 import {Request} from "./src/interface/Request";
 import {insertData} from "./src/crud/insert";
+import {loadData} from "./src/crud/load";
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyStructuredResultV2> => {
 
@@ -10,6 +11,9 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
             case "POST":
                 const request = JSON.parse(event.body) as unknown as Request
                 return insertData(request)
+
+            case "GET":
+                return loadData()
 
             default:
                 let apiResponse = {
